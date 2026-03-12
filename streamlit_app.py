9bbbe55ab02b7356acd645d70599f2c74775da43
becooper24 <becooper@gmaIl.com>
2026-03-12 13:50:49 -0400

    Update streamlit_app.py
    setup streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ if ingredients_list:
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
+
  
     #st.write(my_insert_stmt)
     #st.stop()
@@ -44,7 +45,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
